020eafd756cbc7a36e01bbc75c569c25e3598cb5
Andrea <andrea@example.com>
2026-07-25 17:28:43 +0200

    add wallet page

--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -11,7 +11,6 @@ const [tickets,setTickets]=useState([]);
 const [loading,setLoading]=useState(true);
 const [showModal,setShowModal]=useState(false);
 const [stats,setStats]=useState(null);
-const [loading,setLoading]=useState(false);
 
 
 useEffect(()=>{
