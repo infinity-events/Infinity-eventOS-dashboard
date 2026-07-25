b42fb09482d836f19afdbc1ddded388001a2d6ad
Andrea <andrea@example.com>
2026-07-25 13:07:03 +0200

    Fix dashboard route and Firebase domain

--- src/main.jsx
+++ src/main.jsx
@@ -1,50 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter} from "react-router-dom"
 
 import './index.css'
 import App from './App.jsx'
 
-import {
-FestivalProvider
-} from "./contexts/FestivalContext.jsx"
+import {FestivalProvider} from "./contexts/FestivalContext.jsx"
+import {TicketProvider} from "./contexts/TicketContext.jsx"
+import {AuthProvider} from "./contexts/AuthContext.jsx";
 
-import {
-TicketProvider
-} from "./contexts/TicketContext.jsx"
-
-import {
-AuthProvider
-} from "./contexts/AuthContext.jsx";
-
-
-createRoot(
-document.getElementById('root')
-).render(
-
+createRoot(document.getElementById('root')).render(
 <StrictMode>
 
+<BrowserRouter>
 
 <AuthProvider>
 
-
 <FestivalProvider>
-
 
 <TicketProvider>
 
-
-<App />
-
+<App/>
 
 </TicketProvider>
 
-
 </FestivalProvider>
-
 
 </AuthProvider>
 
+</BrowserRouter>
 
 </StrictMode>
-
 )
