c21571642ba0f5bb78e3df73f731efc31d5792b5
Andrea <andrea@example.com>
2026-07-25 12:58:29 +0200

    modifiche domini

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
 import DashboardLayout from "./layouts/DashboardLayout";
 import Dashboard from "./pages/Dashboard";
@@ -16,23 +17,11 @@ return (
 <BrowserRouter>
 
 <Routes>
-
-<Route element={<DashboardLayout/>}>
-
+<Route path="/" element={<Navigate to="/dashboard" replace/>}/>
 <Route path="/dashboard" element={<Dashboard/>}/>
-
-<Route path="/festivals" element={<Festivals/>}/>
-
-<Route path="/tickets"element={<Tickets/>}/>
-
-<Route path="/wristbands"element={<Wristbands/>}/>
-
-<Route path="/participants" element={<Participants/>}/>
-
+<Route path="/tickets" element={<Tickets/>}/>
+<Route path="/wristbands" element={<Wristbands/>}/>
 <Route path="/nfc/register" element={<NFCRegister/>}/>
-
-</Route>
-
 </Routes>
 
 </BrowserRouter>
