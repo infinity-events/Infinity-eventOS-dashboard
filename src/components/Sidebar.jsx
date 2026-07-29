e5b0b99caba29ea98c0bd0c257241c9f8d44ec02
Andrea <andrea@example.com>
2026-07-29 14:04:23 +0200

    Update favicon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -87,7 +87,7 @@ font-bold
 mb-10
 ">
 
-<img src="/favicon.svg" alt="logo-dash" style="fill: #fff; width: 16; height: 16;"/> Infinity EventOS
+<img src="/favicon.svg" alt="logo-dash" /> Infinity EventOS
 
 </h1>
 
